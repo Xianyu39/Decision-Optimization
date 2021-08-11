a7ce27f75a04aaad7f42ecbfd844962512954e1b
Xianyu <fanghejin@qq.com>
2021-08-12 00:08:10 +0800

    visualize and test

--- Problem1_Farm/src/main.py
+++ Problem1_Farm/src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import search as sc
+import matplotlib.pyplot as plt
 
 plants = ('wheat','corn','vegetable','friut')
 
@@ -7,7 +8,25 @@ prod = np.array([[500,550,630,1000,800,700],
                  [800,700,600,950,90,930],
                  [1200,1040,980,860,880,780],
                  [1000,960,840,650,600,700]])
+lands = (42,56,44,39,60,59)
+plants = (76,88,40,96)
+lenList = (10,60,110,160,210,260)
+iterations = 800
 
-ts = sc.TS((42,56,44,39,60,59),(76,88,40,96),prod)
-ini = np.zeros((4,6))
-ts.adjacentSolution(ini)
+ini = np.ones((4,6))
+fig, ax=plt.subplots()
+
+print('Calculating...')
+for l in lenList:
+    ag = sc.TS(lands, plants, prod, l)
+    ag.getOptimalSolution(ini, iterations)
+    ax.plot(ag.axesX, ag.axesY, label='Tabu list length = '+str(l))
+
+ax.set_title('Production-Iteration Chart')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Production')
+ax.legend()
+ax.grid(axis='both')
+
+print('Calculation done!')
+plt.show()
